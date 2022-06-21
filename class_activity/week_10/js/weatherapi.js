c6fb9666683f94c2c7daf4a228779bce75f7b113
Oculto15 <93210080+Oculto15@users.noreply.github.com>
2022-06-21 09:12:40 -0600

    weather

---
+++ class_activity/week_10/js/weatherapi.js
@@ -0,0 +1,20 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=7ab1d10dc93618cf38fe46aa0435107d";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data); // this is temporary for development only
+    currentTemp.innerHTML = `<strong>${data.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const desc = data.weather[0].description;
+
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", desc);
+    captionDesc.textContent = desc;
+  });
